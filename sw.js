aed78c332d0c3fd25c53cb262e3bb31a0ae76cb9
ToastidTech <toastidtechllc@gmail.com>
2026-09-13 11:13:53 -0500

    Bust cache for new entitlement policy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cope-v36';
-const ASSETS = ['./','./index.html','./manifest.json','./lead-capture.js','./access-gate.js','./logo-192.png','./logo-512.png','./splash-logo.png','./Copeai-logo.png'];
+const CACHE = 'cope-v37';
+const ASSETS = ['./','./index.html','./manifest.json','./lead-capture.js','./access-gate.js','./promo-policy.js','./logo-192.png','./logo-512.png','./splash-logo.png','./Copeai-logo.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
