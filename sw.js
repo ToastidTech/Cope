5e6412bd11a1c687c70a0f84759047bde6b587c2
ToastidTech <toastidtechllc@gmail.com>
2026-09-25 11:38:27 -0500

    Bump SW cache for GA4 events update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cope-v41';
+const CACHE = 'cope-v42';
 const ASSETS = ['./','./index.html','./manifest.json','./lead-capture.js','./access-gate.js','./promo-policy.js','./navigation-fix.js','./logo-192.png','./logo-512.png','./splash-logo.png','./Copeai-logo.png'];
 
 self.addEventListener('install', e => {
